670dccb80cb802b95d84e1f07b0c484197269251
shobhan <shobhan2211@gmail.com>
2026-03-01 19:17:46 +0530

    my second commit

--- Pattern.java
+++ Pattern.java
@@ -27,6 +27,7 @@ public class Pattern {
         int n=5;
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
+                //this is my code for pattern 3
                 //if(i==0 || j==0 || i==4 || j==4)
                 //if((i==0 || i==n-1) && j!=0 || (j==0 && i!=0 && i!=n-1))
                 if(((i==0 || i==n-1) && j!=0) || (j==0 && i>0 && i<n-1) || i==n/2)
