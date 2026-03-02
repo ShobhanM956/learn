2a624d430a9b82818a74182b7f1911acb494dd13
Shobhan Mondal <shobhan2211@gmail.com>
2026-03-03 00:46:32 +0530

    eclipse Exception Handling

---
+++ eclipse/ExceptionHandling/src/com/shobhanm956/git/LaunchEH1.java
@@ -0,0 +1,13 @@
+package com.shobhanm956.git;
+
+public class LaunchEH1 {
+	
+	public static void main(String[] args)
+	{
+		int a=100;
+		int b=0;
+		int result=a/b;
+		System.out.println(result);
+	}
+
+}
